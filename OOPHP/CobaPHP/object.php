4e480e79a3bb699c3c81e62e4223611ffaf3eac7
Naufaliaaa <naufalzul47@gmail.com>
2024-11-04 10:27:00 +0700

    Finising OOP & MVC

---
+++ OOPHP/CobaPHP/object.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Coba {
+
+
+
+}
+// Membuat Object instance dari class
+$a = new coba ();
+$b = new coba ();
+
+
+
+?>
---
+++ OOPHP/CobaPHP/produk.php
@@ -0,0 +1,18 @@
+<?php 
+
+
+//Jualan Produk 
+//Komik
+//Game
+class Produk {
+
+
+
+}
+// Membuat Object instance dari class
+$Produk1 = new Produk ();
+$Produk2 = new Produk ();
+
+
+
+?>
